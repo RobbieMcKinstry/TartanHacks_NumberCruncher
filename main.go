d8ebb4bae167f5ee0b295e1346117e0e87575bdb
Robbie McKinstry <bravenewprince@gmail.com>
2015-02-07 08:12:34 +0000

    Adding some datatypes and outlines

--- main.go
+++ main.go
@@ -2,10 +2,44 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"log"
+
 	"./smoother"
 )
 
 func main() {
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", indexHandler)
+
+	http.Handle("/", r)
 	fmt.Println("Hello world")
-	smoother.Smooth()
+	log.Fatal(http.ListenAndServe())
 }
+
+type Data struct {
+	BloombergData []int `json:"data"`
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+
+	req.ParseForm()
+	for key, _ := range req.Form {
+		log.Println(key)         //LOG: {"test": "that"}
+		data := &Data{}
+		err := json.Unmarshal([]byte(key), data)
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}
+
+	result := smoother.Smooth(data.BloombergData)
+	jsonResult, err := json.Marshal(result)
+	if err != nil {
+		log.Println(err)
+	}
+	return jsonResult
+
+}
+
--- smoother/smoother.go
+++ smoother/smoother.go
@@ -2,6 +2,15 @@ package smoother
 
 import "fmt"
 
+type ReturnInfo struct {
+	Symbol string `json:"symbol"`
+	Current string `json:"current"`
+	Tomorrow string `json:"tomorrow"`
+	AmountIncrease string `json:"amount_increase"`
+	PercentIncrease string `json:"percent_increase"`
+	MeanSquaredError string `json:"mse"`
+}
+
 func Smooth() {
 	fmt.Println("Goodbye world")
 }
